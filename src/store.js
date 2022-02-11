3c9397bd7c3191c9742e4ca7efaafacc490413bd
nhiph <phunghuynhnhi797@gmail.com>
2022-02-11 15:32:17 +0700

    first commit

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import tutorialReducer from './slices/tutorials';
+const reducer = {
+  tutorials: tutorialReducer
+}
+const store = configureStore({
+  reducer: reducer,
+  devTools: true,
+})
+export default store;
